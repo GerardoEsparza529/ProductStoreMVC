d62d87fd0a064f8100437ff16ced79ec6d0678eb
Gerardo Esparza <95262238+GerardoEsparza529@users.noreply.github.com>
2025-04-13 15:19:08 -0600

    Agregar archivos de proyecto.

---
+++ BestStoreMVC/Services/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BestStoreMVC.Services
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
